1478f5a44ca7447829a03017a5da63382fd6c53e
Mike Dunlop <mike@conceptarts.com>
2017-10-30 11:49:09 -0700

    updated noiseAnimated function

--- src/filters/adjust/noiseanimated.js
+++ src/filters/adjust/noiseanimated.js
@@ -4,31 +4,42 @@
  * @param amount   0 to 1 (0 for no effect, 1 for maximum noise)
  * @param time     for animation
  */
-function noiseAnimated(amount,time) {
+function noiseAnimated(amount,randNum) {
+    
+    /*
+    vec2 uv = texCoord;\
+    float strength = 16. * amount;\
+    float x = (uv.x + 4.0 ) * (uv.y + 4.0 ) + time * 0.1;\
+    vec4 grain = vec4(mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01)-0.005) * strength;\
+    
+    color.r += diff;\
+    color.g += diff;\
+    color.b += diff;\
+    */
+
     gl.noiseAnimated = gl.noiseAnimated || new Shader(null, '\
         uniform sampler2D texture;\
         uniform float amount;\
-        uniform float time;\ 
-        uniform vec3 resolution;\ 
+        uniform float time;\
         varying vec2 texCoord;\
+        uniform vec2 texSize;\
         float rand(vec2 co) {\
             return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);\
         }\
         void main() {\
             vec4 color = texture2D(texture, texCoord);\
-            vec2 uv = texCoord.xy / resolution.xy;\
-            uv.y = 1.0 - uv.y;\
-            float strength = 16.0 * amount;\
-            float x = (uv.x + 4.0 ) * (uv.y + 4.0 ) * (time * 10.0);\
-            vec4 grain = vec4(mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01)-0.005) * strength;\
-            grain = 1.0 - grain;\
-            gl_FragColor = color * grain;\
+            float seed = (rand(texCoord) - 0.5 + time);\
+            float strength = 50.0 * amount;\
+            vec4 grain = vec4(mod((mod(seed, 13.0) + 1.0) * (mod(seed, 123.0) + 1.0), 0.01)-0.005) * strength;\
+            \
+            gl_FragColor = color - grain;\
         }\
     ');
 
     simpleShader.call(this, gl.noiseAnimated, {
         amount: clamp(0, amount, 1),
-        time: time
+        time: typeof randNum !== 'undefined' ? parseFloat(randNum) || 0 :  1000 * Math.random(),
+        texSize: [this.width, this.height]
     });
 
     return this;
